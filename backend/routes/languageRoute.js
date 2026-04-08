14608986b56f7ffe737bd7ec58119feeae05cbdf
anujeshyadav <sveltosehr@gmail.com>
2026-04-08 14:43:41 +0530

    removed backend folder

--- backend/routes/languageRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const languageController = require("../controllers/language.controller");
-
-router.get("/get-all", languageController.getAllLanguages);
-
-module.exports = router;
